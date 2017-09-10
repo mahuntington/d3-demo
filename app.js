4526aa395fe5afd2f4ce6da040f2949b3fd7c438
Matt Huntington <matt.huntington@gmail.com>
2017-09-10 00:52:38 -0400

    run data

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 var WIDTH = 800;
 var HEIGHT = 600;
 
+var runs = [
+    {
+        id: 1,
+        date: '10/1/2017',
+        distance: 5.2
+    },
+    {
+        id: 2,
+        date: '10/2/2017',
+        distance: 6.4
+    },
+    {
+        id: 3,
+        date: '10/3/2017',
+        distance: 8.7
+    }
+]
+
 d3.select('svg')
     .style('width', WIDTH)
     .style('height', HEIGHT);
