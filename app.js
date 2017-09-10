12326c5b96625d2b36189f7dec8c38f5027433c6
Matt Huntington <matt.huntington@gmail.com>
2017-09-10 01:32:27 -0400

    time formatting

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ var HEIGHT = 600;
 var runs = [
     {
         id: 1,
-        date: '10/1/2017',
+        date: 'October 1, 2017',
         distance: 5.2
     },
     {
         id: 2,
-        date: '10/2/2017',
+        date: 'October 2, 2017',
         distance: 6.4
     },
     {
         id: 3,
-        date: '10/3/2017',
+        date: 'October 3, 2017',
         distance: 8.7
     }
 ]
@@ -36,5 +36,7 @@ var xScale = d3.scaleTime();
 xScale.range([0,WIDTH]);
 xScale.domain([new Date('2017-10-1'), new Date('2017-10-31')]);
 
-console.log(xScale.domain());
-console.log(xScale.range());
+var parseTime = d3.timeParse("%B%e, %Y");
+console.log(parseTime('October 2, 2017'));
+var formatTime = d3.timeFormat("%B%e, %Y");
+console.log(formatTime(new Date('2017-10-1')));
